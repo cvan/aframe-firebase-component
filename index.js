35f0d5efabf52908916f4619baaad67190d73c4a
Kevin Ngo <me@ngokevin.com>
2016-05-18 16:41:54 -0700

    broadcast component

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
 
+/**
+ * Firebase system.
+ */
 AFRAME.registerSystem('firebase', {
   init: function () {
     var sceneEl = this.sceneEl;
@@ -13,12 +16,39 @@ AFRAME.registerSystem('firebase', {
   }
 });
 
+/**
+ * Data holder for the scene.
+ */
 AFRAME.registerComponent('firebase', {
   schema: {
-    // For the scene until we have system schemas.
     apiKey: {type: 'string'},
     authDomain: {type: 'string'},
     databaseURL: {type: 'string'},
     storageBucket: {type: 'string'}
   }
 });
+
+/**
+ * Broadcast.
+ */
+AFRAME.registerComponent('firebase-broadcast', {
+  schema: {
+    interval: {default: 10},
+    send: {default: ['position', 'rotation']}
+  },
+
+  init: function () {
+    var el = this.el;
+    var system = el.systems.firebase;
+    system.registerBroadcast(el, this.data);
+  }
+});
+
+function guid() {
+  var text = '';
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (var i = 0; i < 5; i++) {
+    text += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return text;
+}
