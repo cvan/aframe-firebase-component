0210aaca2320cf69bb0d753288ac36832580e44b
Kevin Ngo <me@ngokevin.com>
2016-05-20 21:37:12 -0700

    Updates

--- build.js
+++ build.js
@@ -1,8 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 require('aframe');
+require('aframe-template-component');
 require('../index.js');
 
-},{"../index.js":2,"aframe":3}],2:[function(require,module,exports){
+},{"../index.js":2,"aframe":4,"aframe-template-component":3}],2:[function(require,module,exports){
 require('firebase');
 
 if (typeof AFRAME === 'undefined') {
@@ -191,7 +192,256 @@ AFRAME.registerComponent('firebase-broadcast', {
   }
 });
 
-},{"firebase":4}],3:[function(require,module,exports){
+},{"firebase":5}],3:[function(require,module,exports){
+var debug = AFRAME.utils.debug;
+var extend = AFRAME.utils.extend;
+var templateCache = {};  // Template cache.
+var error = debug('template-component:error');
+var log = debug('template-component:info');
+
+var HANDLEBARS = 'handlebars';
+var JADE = 'jade';
+var MUSTACHE = 'mustache';
+var NUNJUCKS = 'nunjucks';
+var HTML = 'html';
+
+var LIB_LOADED = {};
+LIB_LOADED[HANDLEBARS] = !!window.Handlebars;
+LIB_LOADED[JADE] = !!window.jade;
+LIB_LOADED[MUSTACHE] = !!window.Mustache;
+LIB_LOADED[NUNJUCKS] = !!window.nunjucks;
+
+var LIB_SRC = {};
+LIB_SRC[HANDLEBARS] = 'https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js';
+LIB_SRC[JADE] = 'https://cdnjs.cloudflare.com/ajax/libs/jade/1.11.0/jade.min.js';
+LIB_SRC[MUSTACHE] = 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min.js';
+LIB_SRC[NUNJUCKS] = 'https://cdnjs.cloudflare.com/ajax/libs/nunjucks/2.3.0/nunjucks.min.js';
+
+AFRAME.registerComponent('template', {
+  schema: {
+    insert: {
+      // insertAdjacentHTML.
+      default: 'beforeend'
+    },
+    type: {
+      default: ''
+    },
+    src: {
+      // Selector or URL.
+      default: ''
+    },
+    data: {
+      default: ''
+    }
+  },
+
+  update: function () {
+    var data = this.data;
+    var fetcher = data.src[0] === '#' ? fetchTemplateFromScriptTag : fetchTemplateFromXHR;
+    var templateCacheItem = templateCache[data.src];
+
+    if (templateCacheItem) {
+      this.renderTemplate(templateCacheItem);
+      return;
+    }
+
+    fetcher(data.src, data.type).then(this.renderTemplate.bind(this));
+  },
+
+  renderTemplate: function (templateCacheItem) {
+    var el = this.el;
+    var data = this.data;
+    var templateData = {};
+
+    Object.keys(el.dataset).forEach(function convertToData (key) {
+      templateData[key] = el.dataset[key];
+    });
+    if (data.data) {
+      templateData = extend(templateData, el.getComputedAttribute(data.data));
+    }
+
+    var renderedTemplate = renderTemplate(templateCacheItem.template, templateCacheItem.type,
+                                          templateData);
+    el.insertAdjacentHTML(data.insert, renderedTemplate);
+  }
+});
+
+/**
+ * Helper to compile template, lazy-loading the template engine if needed.
+ */
+function compileTemplate (src, type, templateStr) {
+  return new Promise(function (resolve) {
+    injectTemplateLib(type).then(function () {
+      templateCache[src] = {
+        template: getCompiler(type)(templateStr.trim()),
+        type: type
+      };
+      resolve(templateCache[src]);
+    });
+  });
+}
+
+function renderTemplate (template, type, context) {
+  switch (type) {
+    case HANDLEBARS: {
+      return template(context);
+    }
+    case JADE: {
+      return template(context);
+    }
+    case MUSTACHE: {
+      return Mustache.render(template, context);
+    }
+    case NUNJUCKS: {
+      return template.render(context);
+    }
+    default: {
+      // If type not specified, assume raw HTML and no templating needed.
+      return template;
+    }
+  }
+}
+
+/**
+ * Cache and compile templates.
+ */
+function fetchTemplateFromScriptTag (src, type) {
+  var compiler;
+  var scriptEl = document.querySelector(src);
+  var scriptType = scriptEl.getAttribute('type');
+  var templateStr = scriptEl.innerHTML;
+
+  // Try to infer template type from <script type> if type not specified.
+  if (!type) {
+    if (!scriptType) {
+      throw new Error('Must provide `type` attribute for <script> templates (e.g., handlebars, jade, nunjucks, html)');
+    }
+    if (scriptType.indexOf('handlebars') !== -1) {
+      type = HANDLEBARS;
+    } else if (scriptType.indexOf('jade') !== -1) {
+      type = JADE
+    } else if (scriptType.indexOf('mustache') !== -1) {
+      type = MUSTACHE;
+    } else if (scriptType.indexOf('nunjucks') !== -1) {
+      type = NUNJUCKS
+    } else if(scriptType.indexOf('html') !== -1) {
+      type = HTML;
+    } else {
+      error('Template type could not be inferred from the script tag. Please add a type.');
+      return;
+    }
+  }
+
+  return new Promise(function (resolve) {
+    compileTemplate(src, type, templateStr).then(function (template) {
+      resolve(template, type);
+    });
+  });
+}
+
+function fetchTemplateFromXHR (src, type) {
+  return new Promise(function (resolve) {
+    var request;
+    request = new XMLHttpRequest();
+    request.addEventListener('load', function () {
+      // Template fetched. Use template.
+      compileTemplate(src, type, request.response).then(function (template) {
+        resolve(template, type);
+      });
+    });
+    request.open('GET', src);
+    request.send();
+  });
+}
+
+/**
+ * Get compiler given type.
+ */
+function getCompiler (type) {
+  switch (type) {
+    case HANDLEBARS: {
+      return compileHandlebarsTemplate;
+    }
+    case JADE: {
+      return compileJadeTemplate;
+    }
+    case MUSTACHE: {
+      return compileHandlebarsTemplate;
+    }
+    case NUNJUCKS: {
+      return compileNunjucksTemplate;
+    }
+    default: {
+      // If type not specified, assume raw HTML and no templating needed.
+      return function (str) { return str; };
+    }
+  }
+}
+
+function compileHandlebarsTemplate (templateStr) {
+  return Handlebars.compile(templateStr);
+}
+
+function compileJadeTemplate (templateStr) {
+  return jade.compile(templateStr);
+}
+
+function compileMustacheTemplate (templateStr) {
+  Mustache.parse(templateStr);
+  return templateStr;
+}
+
+function compileNunjucksTemplate (templateStr) {
+  return nunjucks.compile(templateStr);
+}
+
+function injectTemplateLib (type) {
+  return new Promise(function (resolve) {
+    // No lib injection required.
+    if (!type || type === 'html') { return resolve(); }
+
+    var scriptEl = LIB_LOADED[type];
+
+    // Engine loaded.
+    if (LIB_LOADED[type] === true) { return resolve(); }
+
+    // Start lazy-loading.
+    if (!scriptEl) {
+      scriptEl = document.createElement('script');
+      LIB_LOADED[type] = scriptEl;
+      scriptEl.setAttribute('src', LIB_SRC[type]);
+      log('Lazy-loading %s engine. Please add <script src="%s"> to your page.',
+          type, LIB_SRC[type]);
+      document.body.appendChild(scriptEl);
+    }
+
+    // Wait for onload, whether just injected or already lazy-loading.
+    var prevOnload = scriptEl.onload || function () {};
+    scriptEl.onload = function () {
+      prevOnload();
+      LIB_LOADED[type] = true;
+      resolve();
+    };
+  });
+};
+
+AFRAME.registerComponent('template-set', {
+  schema: {
+    on: {type: 'string'},
+    src: {type: 'string'},
+    data: {type: 'string'}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener(data.on, function () {
+      el.setAttribute('template', {src: data.src, data: data.data});
+    });
+  }
+});
+
+},{}],4:[function(require,module,exports){
 (function (global){
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AFRAME = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
@@ -61095,7 +61345,7 @@ module.exports = getWakeLock();
 
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 (function (global){
 /*! @license Firebase v3.0.0
     Build: 3.0.0-rc.9
