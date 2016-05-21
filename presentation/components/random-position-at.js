d0cdffcc584f19f550c1d8107fcdf28e33118a7b
Kevin Ngo <me@ngokevin.com>
2016-05-20 22:25:01 -0700

    Updates

---
+++ presentation/components/random-position-at.js
@@ -0,0 +1,29 @@
+/**
+ * Set random position at one of the entities specified by `selector` with optional `offset`.
+ */
+AFRAME.registerComponent('random-position-at', {
+  dependencies: ['position'],
+
+  schema: {
+    offset: {type: 'vec3'},
+    selector: {type: 'string'}
+  },
+
+  update: function () {
+    var data = this.data;
+    var el = this.el;
+
+    setTimeout(function () {
+      var entities = el.sceneEl.querySelectorAll(data.selector);
+      var entity = entities[Math.floor(Math.random() * entities.length)];
+      var position = entity.getComputedAttribute('position');
+      var parentPosition = entity.parentNode.getComputedAttribute('position');
+
+      el.setAttribute('position', {
+        x: position.x + parentPosition.x + data.offset.x,
+        y: position.y + parentPosition.y + data.offset.y,
+        z: position.z + parentPosition.z + data.offset.z
+      });
+    }, 100);
+  }
+});
